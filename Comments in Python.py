6f14c4935500a02b1e8754b90aa919b8b08eda46
unknown <gfram@hotmail.com>
2019-12-18 20:58:54 +0000

    Updated Applications

--- Comments in Python.py
+++ Comments in Python.py
@@ -1,8 +1,5 @@
-Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
 >>> #this is a comment
 >>> print("hi")
 hi
 >>> ##This is a simple comment in Python!
->>> 
+>>> print("With this I will print out some simple text")
---
+++ Docstring in Python.py
@@ -0,0 +1,13 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> """
+Using docsring as a comment.
+This code divides 2 numbers
+"""
+'\nUsing docsring as a comment.\nThis code divides 2 numbers\n'
+>>> x=8
+>>> y=4
+>>> z=x/y
+>>> print(z)
+2.0
+>>> 
